f60dc9094867e618704e7d1f696b62a13b239e8b
Vinayak Mathur <vinayakrulez@gmail.com>
2022-09-22 01:11:20 -0400

    Stock data pulled

---
+++ server/src/StockPullerUpdater.py
@@ -0,0 +1,44 @@
+import yfinance as yf
+import pandas as pd
+
+def populate(tots = list(pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")[0]["Symbol"])): 
+    
+    tots = [x for x in tots if "." not in x]
+    
+    data = yf.download(  # or pdr.get_data_yahoo(...
+        # tickers list or string as well
+        tickers = tots,
+
+        # use "period" instead of start/end
+        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+        # (optional, default is '1mo')
+        period = "10y",
+
+        # fetch data by interval (including intraday if period < 60 days)
+        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        # (optional, default is '1d')
+        interval = "1d",
+
+        # group by ticker (to access via data['SPY'])
+        # (optional, default is 'column')
+        group_by = 'column',
+
+        # adjust all OHLC automatically
+        # (optional, default is False)
+        auto_adjust = True,
+
+        # download pre/post regular market hours data
+        # (optional, default is False)
+        prepost = False,
+
+        # use threads for mass downloading? (True/False/Integer)
+        # (optional, default is True)
+        threads = True,
+
+        # proxy URL scheme use use when downloading?
+        # (optional, default is None)
+        proxy = None
+    )
+    data["Close"].to_csv("SP500.csv")
+
+populate()
