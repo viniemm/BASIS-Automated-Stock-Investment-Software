a1360739340ce633847d557e0b140faf80daf5a6
Hunter Welch <35479403+hwelch@users.noreply.github.com>
2022-10-11 21:44:58 -0400

    Added Questionnaire to index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import Dashboard from './Dashboard/Dashboard';
 import About from './About/About';
 import Home from './Home/Home'
+import Questionnaire from '../components/Questionnaire/Questionnaire';
 
-export {Dashboard, About, Home}
+export {Dashboard, About, Home, Questionnaire}
