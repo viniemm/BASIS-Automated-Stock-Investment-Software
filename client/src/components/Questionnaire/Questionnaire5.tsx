b83021e544093adb228c09d1fd18018b9738df9c
Hunter Welch <35479403+hwelch@users.noreply.github.com>
2022-10-17 20:08:30 -0400

    Update Questionnaire5.tsx
    Put in correct industries as options

--- client/src/components/Questionnaire/Questionnaire5.tsx
+++ client/src/components/Questionnaire/Questionnaire5.tsx
@@ -3,24 +3,20 @@ import { Dropdown } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 
 const options = [
-  { key: 'angular', text: 'Angular', value: 'angular' },
-  { key: 'css', text: 'CSS', value: 'css' },
-  { key: 'design', text: 'Graphic Design', value: 'design' },
-  { key: 'ember', text: 'Ember', value: 'ember' },
-  { key: 'html', text: 'HTML', value: 'html' },
-  { key: 'ia', text: 'Information Architecture', value: 'ia' },
-  { key: 'javascript', text: 'Javascript', value: 'javascript' },
-  { key: 'mech', text: 'Mechanical Engineering', value: 'mech' },
-  { key: 'meteor', text: 'Meteor', value: 'meteor' },
-  { key: 'node', text: 'NodeJS', value: 'node' },
-  { key: 'plumbing', text: 'Plumbing', value: 'plumbing' },
-  { key: 'python', text: 'Python', value: 'python' },
-  { key: 'rails', text: 'Rails', value: 'rails' },
-  { key: 'react', text: 'React', value: 'react' },
-  { key: 'repair', text: 'Kitchen Repair', value: 'repair' },
-  { key: 'ruby', text: 'Ruby', value: 'ruby' },
-  { key: 'ui', text: 'UI Design', value: 'ui' },
-  { key: 'ux', text: 'User Experience', value: 'ux' },
+  { key: 'oil', text: 'Oil and Gas', value: 'oil' },
+  { key: 'food', text: 'Food and Beverages', value: 'food' },
+  { key: 'pharm', text: 'Pharmaceutical', value: 'pharm' },
+  { key: 'realEstate', text: 'Real Estate', value: 'realEstate' },
+  { key: 'tech', text: 'Technology', value: 'tech' },
+  { key: 'services', text: 'Services', value: 'services' },
+  { key: 'util', text: 'Utilities', value: 'util' },
+  { key: 'machinery', text: 'Machinery', value: 'machinery' },
+  { key: 'retail', text: 'Retail', value: 'retail' },
+  { key: 'metals', text: 'Metals', value: 'metals' },
+  { key: 'accessories', text: 'Accessories', value: 'accessories' },
+  { key: 'travel', text: 'Travel', value: 'travel' },
+  { key: 'insurance', text: 'Insurance', value: 'insurance' },
+  { key: 'banks', text: 'Banks, Business, and Finance', value: 'banks' },
 ]
 
 const DropdownMultipleSelection = () => (
