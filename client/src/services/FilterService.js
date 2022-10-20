85d633b5a9808b10c4d2a38a0aff5a706137bf09
whaskey <71858348+whaskey@users.noreply.github.com>
2022-10-19 21:40:40 -0400

    Create FilterService.js

---
+++ client/src/services/FilterService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { Console } from 'console';
+// idk what goes here
+
+//axios call here
+// export const sendFilter = async (filterDetails: )  => {
+//     let response = await axios
+//         .post('/api/filter/', JSON.stringify(filterDetails));
+//         if (response.status === 200) {
+//             console.log("Nice");
+//         }
+// };
