ab5a8457542f5fe44ab3273b2ee8478c0cfbc7d0
whaskey <71858348+whaskey@users.noreply.github.com>
2022-12-06 17:44:33 -0500

    structure for dashboard page

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Box, Button, Card, Container, Grid, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 export default function Dashboard() {
   return (
     <div>
       <h2>Dashboard</h2>
+      <Grid container spacing={4}>
+        <Grid item xs={8}>
+          <Card variant="outlined">
+            <Typography fontSize="xl" sx={{ mb: 0.5 }}>
+              Your Profile
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          
+        </Grid>
+        <Grid item xs={8}>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="outlined">
+              <Link to="/questionnaire"> Make a New Portfolio </Link>
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
