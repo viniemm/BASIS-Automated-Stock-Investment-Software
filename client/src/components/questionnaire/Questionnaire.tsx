3d60edfce73c10568a53259e074895ed81b5a501
Hunter Welch <35479403+hwelch@users.noreply.github.com>
2022-12-07 12:22:15 -0500

    Made Risk and AmountInvested a dropdown
    made these values a dropdown instead

--- client/src/components/questionnaire/Questionnaire.tsx
+++ client/src/components/questionnaire/Questionnaire.tsx
@@ -40,6 +40,112 @@ const termPeriods = [
   },
 ]
 
+const riskValues = [
+  {
+    key: '1',
+    text: '1',
+    value: '1'
+  },
+  {
+    key: '2',
+    text: '2',
+    value: '2'
+  },
+  {
+    key: '3',
+    text: '3',
+    value: '3'
+  },
+  {
+    key: '4',
+    text: '4',
+    value: '4'
+  },
+  {
+    key: '5',
+    text: '5',
+    value: '5'
+  },
+  {
+    key: '6',
+    text: '6',
+    value: '6'
+  },
+  {
+    key: '7',
+    text: '7',
+    value: '7'
+  },
+  {
+    key: '8',
+    text: '8',
+    value: '8'
+  },
+  {
+    key: '9',
+    text: '9',
+    value: '9'
+  },
+  {
+    key: '10',
+    text: '10',
+    value: '10'
+  },
+]
+
+const investValues = [
+  {
+    key: '1000',
+    text: '1000',
+    value: '1000'
+  },
+  {
+    key: '2000',
+    text: '2000',
+    value: '2000'
+  },
+  {
+    key: '3000',
+    text: '3000',
+    value: '3000'
+  },
+  {
+    key: '4000',
+    text: '4000',
+    value: '4000'
+  },
+  {
+    key: '5000',
+    text: '5000',
+    value: '5000'
+  },
+  {
+    key: '6000',
+    text: '6000',
+    value: '6000'
+  },
+  {
+    key: '7000',
+    text: '7000',
+    value: '7000'
+  },
+  {
+    key: '8000',
+    text: '8000',
+    value: '8000'
+  },
+  {
+    key: '9000',
+    text: '9000',
+    value: '9000'
+  },
+  {
+    key: '10000',
+    text: '10000',
+    value: '10000'
+  },
+]
+
 const options = [
   { key: 'oil', text: 'Oil and Gas', value: 'oil' },
   { key: 'food', text: 'Food and Beverages', value: 'food' },
@@ -86,25 +192,37 @@ export default function Questionnaire({ auth }: QuestionnaireProps) {
         <label>
           <h4>1. How much money are you willing to invest (In $)?</h4>
         </label>
-        <input
-          placeholder="Money Invested"
-          onChange={(input) => {
-            setMoneyInvested(+input.target.value)
+        <Dropdown
+          placeholder='Select Amount to Invest'
+          fluid
+          selection
+          options={investValues}
+          onChange={(e, data) => { 
+            if(data.value == undefined) {
+              data.value = 100
+            }
+            setRiskThreshold(+data.value) 
           }}
-        ></input>
+        />
       </Form.Field>
 
       {/* Question 2 */}
       <Form.Field>
         <label>
-          <h4>2. What is your risk threshold?</h4>
+          <h4>2. What is your risk threshold? (Where 1 is low risk and 10 is high risk tolerance)</h4>
         </label>
-        <input
-          placeholder="Risk Threshold"
-          onChange={(input) => {
-            setRiskThreshold(+input.target.value)
+        <Dropdown required
+          placeholder='Select Risk'
+          fluid
+          selection
+          options={riskValues}
+          onChange={(e, data) => { 
+            if(data.value == undefined) {
+              data.value = 0
+            }
+            setRiskThreshold(+data.value) 
           }}
-        ></input>
+        />
       </Form.Field>
 
       {/* Question 3 */}
